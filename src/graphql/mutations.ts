cad67bb666dc4c52bb23f326cdfd9939805f0ec7
JuJarazo <julianjarazo94@gmail.com>
2023-01-15 23:01:52 -0300

    Add order mutation

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,2 +1,9 @@
-// Here we put mutations. Remove next line
-export {};
+import { gql } from '@apollo/client';
+
+export const ADD_ITEM_TO_ORDER = gql`
+  mutation AddItemToOrder($productVariantId: String!) {
+    addItemToOrder(productVariantId: $productVariantId, quantity: 1) {
+      __typename
+    }
+  }
+`;
