cfe2436d7162392780b907f3165729d0e99e6332
JuJarazo <julianjarazo94@gmail.com>
2023-01-16 09:42:42 -0300

    Fix mutation var type

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const ADD_ITEM_TO_ORDER = gql`
-  mutation AddItemToOrder($productVariantId: String!) {
+  mutation AddItemToOrder($productVariantId: ID!) {
     addItemToOrder(productVariantId: $productVariantId, quantity: 1) {
       __typename
     }
